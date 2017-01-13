f19455b991807fe3c1206cdd5e53f265198a9d62
GeoAlexa <geo.gabriel.alexa@gmail.com>
2017-01-13 18:24:13 +0200

    Varianta 2

--- Convertor/main.cpp
+++ Convertor/main.cpp
@@ -51,7 +51,169 @@ int main()
             cout<<"Mile: "<<mile<<"\n";
             cout<<"Yarzi: "<<yarzi<<"\n";
         }
+        else if(alege_marimea==2)
+        {
+            cout<<"Centimetri: ";
+            cin>>centimetri;
+            cout<<"\n";
 
+            metri=centimetri/100;
+            milimetri=metri*1000;
+            kilometri=metri*0.001;
+            toli=metri/0.0254;
+            picioare=metri*3.28083;
+            mile=metri*0.000621371192;
+            yarzi=metri/0.9144;
+
+            cout<<"Milimetri: "<<milimetri<<"\n";
+            cout<<"Metri: "<<metri<<"\n";
+            cout<<"Kilometri: "<<kilometri<<"\n";
+            cout<<"Toli: "<<toli<<"\n";
+            cout<<"Picioare: "<<picioare<<"\n";
+            cout<<"Mile: "<<mile<<"\n";
+            cout<<"Yarzi: "<<yarzi<<"\n";
+
+        }
+
+        else if(alege_marimea==3)
+        {
+            cout<<"Metri: ";
+            cin>>metri;
+            cout<<"\n";
+
+            milimetri=metri*1000;
+            centimetri=metri*100;
+            kilometri=metri*0.001;
+            toli=metri/0.0254;
+            picioare=metri*3.28083;
+            mile=metri*0.000621371192;
+            yarzi=metri/0.9144;
+
+            cout<<"Milimetri: "<<milimetri<<"\n";
+            cout<<"Centimetri: "<<centimetri<<"\n";
+            cout<<"Kilometri: "<<kilometri<<"\n";
+            cout<<"Toli: "<<toli<<"\n";
+            cout<<"Picioare: "<<picioare<<"\n";
+            cout<<"Mile: "<<mile<<"\n";
+            cout<<"Yarzi: "<<yarzi<<"\n";
+        }
+
+        else if(alege_marimea==4)
+        {
+            cout<<"Kilometri: ";
+            cin>>kilometri;
+            cout<<"\n";
+
+            metri=kilometri*1000;
+            milimetri=metri*1000;
+            centimetri=metri*100;
+            toli=metri/0.0254;
+            picioare=metri*3.28083;
+            mile=metri*0.000621371192;
+            yarzi=metri/0.9144;
+
+            cout<<"Milimetri: "<<milimetri<<"\n";
+            cout<<"Centimetri: "<<centimetri<<"\n";
+            cout<<"Metri: "<<metri<<"\n";
+            cout<<"Toli: "<<toli<<"\n";
+            cout<<"Picioare: "<<picioare<<"\n";
+            cout<<"Mile: "<<mile<<"\n";
+            cout<<"Yarzi: "<<yarzi<<"\n";
+        }
+
+        else if(alege_marimea==5)
+        {
+            cout<<"Toli: ";
+            cin>>toli;
+            cout<<"\n";
+
+            metri=toli*0.0254;
+            milimetri=metri*1000;
+            centimetri=metri*100;
+            kilometri=metri/1000;
+            picioare=metri*3.28083;
+            mile=metri*0.000621371192;
+            yarzi=metri/0.9144;
+
+            cout<<"Milimetri: "<<milimetri<<"\n";
+            cout<<"Centimetri: "<<centimetri<<"\n";
+            cout<<"Metri: "<<metri<<"\n";
+            cout<<"Kilometri: "<<kilometri<<"\n";
+            cout<<"Picioare: "<<picioare<<"\n";
+            cout<<"Mile: "<<mile<<"\n";
+            cout<<"Yarzi: "<<yarzi<<"\n";
+        }
+
+        else if(alege_marimea==6)
+        {
+            cout<<"Picioare: ";
+            cin>>picioare;
+            cout<<"\n";
+
+            metri=picioare/3.28083;
+            milimetri=metri*1000;
+            centimetri=metri*100;
+            kilometri=metri/1000;
+            toli=metri/0.0254;
+            mile=metri*0.000621371192;
+            yarzi=metri/0.9144;
+
+            cout<<"Milimetri: "<<milimetri<<"\n";
+            cout<<"Centimetri: "<<centimetri<<"\n";
+            cout<<"Metri: "<<metri<<"\n";
+            cout<<"Kilometri: "<<kilometri<<"\n";
+            cout<<"Toli: "<<toli<<"\n";
+            cout<<"Mile: "<<mile<<"\n";
+            cout<<"Yarzi: "<<yarzi<<"\n";
+        }
+
+        else if(alege_marimea==7)
+        {
+            cout<<"Mile: ";
+            cin>>mile;
+            cout<<"\n";
+
+            metri=mile/0.000621371192;
+            picioare=metri*3.28083;
+            milimetri=metri*1000;
+            centimetri=metri*100;
+            kilometri=metri/1000;
+            toli=metri/0.0254;
+            yarzi=metri/0.9144;
+
+            cout<<"Milimetri: "<<milimetri<<"\n";
+            cout<<"Centimetri: "<<centimetri<<"\n";
+            cout<<"Metri: "<<metri<<"\n";
+            cout<<"Kilometri: "<<kilometri<<"\n";
+            cout<<"Toli: "<<toli<<"\n";
+            cout<<"Picioare: "<<picioare<<"\n";
+            cout<<"Yarzi: "<<yarzi<<"\n";
+        }
+
+         else if(alege_marimea==8)
+        {
+            cout<<"Yarzi: ";
+            cin>>yarzi;
+            cout<<"\n";
+
+            metri=yarzi*0.9144;
+            mile=metri*0.000621371192;
+            picioare=metri*3.28083;
+            milimetri=metri*1000;
+            centimetri=metri*100;
+            kilometri=metri/1000;
+            toli=metri/0.0254;
+
+            cout<<"Milimetri: "<<milimetri<<"\n";
+            cout<<"Centimetri: "<<centimetri<<"\n";
+            cout<<"Metri: "<<metri<<"\n";
+            cout<<"Kilometri: "<<kilometri<<"\n";
+            cout<<"Toli: "<<toli<<"\n";
+            cout<<"Picioare: "<<picioare<<"\n";
+            cout<<"Mile: "<<mile<<"\n";
+        }
+        else
+            cout<<"Valoare invalida";
         return 0;
     }
 }
