a0437c321cf28f68077fe0fc2a4122986dcb91ec
GeoAlexa <geo.gabriel.alexa@gmail.com>
2017-01-13 19:51:49 +0200

    Varianta 3

--- Convertor/main.cpp
+++ Convertor/main.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main()
 {
-    int unitate_de_masura;
+    int unitate_de_masura, alege_marimea;
 
+    cout<<"Convertor"<<"\n"<<"\n";
     cout<<"1. Lungime"<<"\n";
-
+    cout<<"2. Arie"<<"\n";
+    cout<<"3. Temperatura"<<"\n";
     cin>>unitate_de_masura;
     cout<<"\n";
 
     if(unitate_de_masura==1)
     {
-        int alege_marimea;
         long double milimetri=0, centimetri=0, metri=0, kilometri=0, mile=0, toli=0, picioare=0, yarzi=0;
 
         cout<<"1. Milimetri"<<"\n";
@@ -214,6 +215,110 @@ int main()
         }
         else
             cout<<"Valoare invalida";
-        return 0;
     }
+
+    else if(unitate_de_masura==2)
+    {
+        long double metri_patrati=0, ari=0, acri=0, hectare=0, yarzi_patrati=0;
+
+        cout<<"1. Metri patrati"<<"\n";
+        cout<<"2. Ari"<<"\n";
+        cout<<"3. Acri"<<"\n";
+        cout<<"4. Hectare"<<"\n";
+        cout<<"5. Yarzi patrati"<<"\n";
+
+        cin>>alege_marimea;
+        cout<<"\n";
+
+        if(alege_marimea==1)
+        {
+            cout<<"Metri patrati: ";
+            cin>>metri_patrati;
+            cout<<"\n";
+
+            ari=metri_patrati*0.01;
+            acri=metri_patrati*0.0002471053;
+            hectare=metri_patrati*0.0001;
+            yarzi_patrati=metri_patrati*1.19598939404;
+
+            cout<<"Ari: "<<ari<<"\n";
+            cout<<"Acri: "<<acri<<"\n";
+            cout<<"Hectare: "<<hectare<<"\n";
+            cout<<"Yarzi patrati: "<<yarzi_patrati<<"\n";
+        }
+
+        else if(alege_marimea==2)
+        {
+            cout<<"Ari: ";
+            cin>>ari;
+            cout<<"\n";
+
+            metri_patrati=ari/0.01;
+            acri=metri_patrati*0.0002471053;
+            hectare=metri_patrati*0.0001;
+            yarzi_patrati=metri_patrati*1.19598939404;
+
+            cout<<"Metri patrati: "<<metri_patrati<<"\n";
+            cout<<"Acri: "<<acri<<"\n";
+            cout<<"Hectare: "<<hectare<<"\n";
+            cout<<"Yarzi patrati: "<<yarzi_patrati<<"\n";
+        }
+
+        else if(alege_marimea==3)
+        {
+            cout<<"Acri: ";
+            cin>>acri;
+            cout<<"\n";
+
+            metri_patrati=acri/0.0002471053;
+            ari=metri_patrati*0.01;
+            hectare=metri_patrati*0.0001;
+            yarzi_patrati=metri_patrati*1.19598939404;
+
+            cout<<"Metri patrati: "<<metri_patrati<<"\n";
+            cout<<"Ari: "<<ari<<"\n";
+            cout<<"Hectare: "<<hectare<<"\n";
+            cout<<"Yarzi patrati: "<<yarzi_patrati<<"\n";
+        }
+
+        else if(alege_marimea==4)
+        {
+            cout<<"Hectare: ";
+            cin>>hectare;
+            cout<<"\n";
+
+            metri_patrati=hectare/0.0001;
+            ari=metri_patrati*0.01;
+            acri=metri_patrati*0.0002471053;
+            yarzi_patrati=metri_patrati*1.19598939404;
+
+            cout<<"Metri patrati: "<<metri_patrati<<"\n";
+            cout<<"Ari: "<<ari<<"\n";
+            cout<<"Acri: "<<acri<<"\n";
+            cout<<"Yarzi patrati: "<<yarzi_patrati<<"\n";
+        }
+
+        else if(alege_marimea==5)
+        {
+            cout<<"Yarzi patrati: ";
+            cin>>yarzi_patrati;
+            cout<<"\n";
+
+            metri_patrati=yarzi_patrati/1.19598939404;
+            ari=metri_patrati*0.01;
+            acri=metri_patrati*0.0002471053;
+            hectare=metri_patrati*0.0001;
+
+            cout<<"Metri patrati: "<<metri_patrati<<"\n";
+            cout<<"Ari: "<<ari<<"\n";
+            cout<<"Acri: "<<acri<<"\n";
+            cout<<"Hectare: "<<hectare<<"\n";
+        }
+        else
+            cout<<"Alegere invalida";
+    }
+
+
+
+    return 0;
 }
